019ff59f37bff043496f4f753af7e4d9fedfd6f3
キリト先生 <44807029+IronShell1207@users.noreply.github.com>
2024-04-15 21:49:35 +0700

    init

---
+++ SysHelpers/PinvokeWindowMethods.cs
@@ -0,0 +1,30 @@
+﻿namespace SysHelpers
+{
+	using System;
+	using System.Runtime.InteropServices;
+
+	public class PinvokeWindowMethods
+	{
+		[DllImport("kernel32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool AllocConsole();
+
+		[DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+		public static extern uint GetDpiForWindow(IntPtr hwnd);
+		[DllImport("user32")]
+		public static extern int RegisterWindowMessage(string message);
+
+		[DllImport("user32.dll", EntryPoint = "SetWindowLong", SetLastError = true)]
+		public static extern int SetWindowLong32(HandleRef hWnd, int nIndex, int dwNewLong);
+
+		[DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
+		public static extern IntPtr SetWindowLongPtr64(HandleRef hWnd, int nIndex, IntPtr dwNewLong);
+
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern IntPtr SetFocus(IntPtr hWnd);
+
+		[DllImport("User32.dll")]
+		public static extern int SetForegroundWindow(IntPtr point);
+
+	}
+}
---
+++ Utils/DispatcherQueueHelper.cs
@@ -0,0 +1,96 @@
+﻿using CommunityToolkit.WinUI;
+using Microsoft.UI.Dispatching;
+
+namespace Utils.Helpers;
+
+#if !WPF
+
+using System;
+using System.Threading.Tasks;
+
+#if NETFX_CORE
+using Windows.System;
+using Microsoft.Toolkit.Uwp;
+#endif
+
+#if WIN_APP_SDK
+using CommunityToolkit.WinUI;
+using Microsoft.UI.Dispatching;
+#endif
+
+
+public static class DispatcherQueueHelper
+{
+
+	private static DispatcherQueue _dispatcherQueue;
+
+	private static string DispatcherExceptionString => $"DispatcherQueue not initialized, add {nameof(SetCurrentThread)} to app constructor";
+
+
+	public static DispatcherQueueTimer CreateTimer(TimeSpan interval)
+	{
+		try
+		{
+			DispatcherQueueTimer timer = _dispatcherQueue.CreateTimer();
+			timer.Interval = interval;
+
+			return timer;
+		}
+		catch (NullReferenceException)
+		{
+			throw new NullReferenceException(DispatcherExceptionString);
+		}
+	}
+
+	public static async Task EnqueueAsync(Action function, DispatcherQueuePriority priority = DispatcherQueuePriority.Normal)
+	{
+		try
+		{
+			await _dispatcherQueue.EnqueueAsync(function, priority);
+		}
+		catch (NullReferenceException)
+		{
+			throw new NullReferenceException(DispatcherExceptionString);
+		}
+	}
+
+	public static async Task<T> EnqueueAsync<T>(Func<T> function, DispatcherQueuePriority priority = DispatcherQueuePriority.Normal)
+	{
+		try
+		{
+			return await _dispatcherQueue.EnqueueAsync(function, priority);
+		}
+		catch (NullReferenceException)
+		{
+			throw new NullReferenceException(DispatcherExceptionString);
+		}
+	}
+
+	public static async Task EnqueueAsync(Func<Task> function, DispatcherQueuePriority priority = DispatcherQueuePriority.Normal)
+	{
+		try
+		{
+			await _dispatcherQueue.EnqueueAsync(function, priority);
+		}
+		catch (NullReferenceException)
+		{
+			throw new NullReferenceException(DispatcherExceptionString);
+		}
+	}
+
+	public static async Task<T> EnqueueAsync<T>(Func<Task<T>> function, DispatcherQueuePriority priority = DispatcherQueuePriority.Normal)
+	{
+		try
+		{
+			return await _dispatcherQueue.EnqueueAsync(function, priority);
+		}
+		catch (NullReferenceException)
+		{
+			throw new NullReferenceException(DispatcherExceptionString);
+		}
+	}
+
+	public static void SetCurrentThread() => _dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+}
+
+#endif
